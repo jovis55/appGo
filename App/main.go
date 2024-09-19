a385a13e95ff8b3d35f0d2131606b565cb68366e
johana <johanapaolapalacio@gmail.com>
2024-09-19 18:36:51 -0500

    CLASES PERO HAY UNOS ERRORES DE IMPORTACION

---
+++ App/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	
+)
+
+func main(){
+
+	// Ejemplo de slice de imágenes
+	imageFiles := []string{"App/images/El Beso.jpg","App/images/El grito del Alma.jpg", "App/images/El Hijo del Hombre.jpg", "" }
+
+	// Cantidad de imágenes a seleccionar aleatoriamente
+	quantity := 2
+
+	// Llamar a la función para obtener imágenes aleatorias sin repetición
+	selectedImages := functions.getImages(imageFiles, quantity)
+
+	// Imprimir las imágenes seleccionadas
+	fmt.Println("Imágenes seleccionadas aleatoriamente:", selectedImages)
+	//DEVOLVER UN  MENSAJE A LA PETICION DEL CLIENTE
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
+		fmt.Fprintln(rw, "Hola Mundo")
+	})
+		
+	fmt.Println("El servidor esta cotrriendo en puerto 3000")
+	http.ListenAndServe("localhost:3000", nil)
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+module App
+
+
+go 1.23.1
